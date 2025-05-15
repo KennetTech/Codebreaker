2ee9d2ab5fbcfd455d604d084d954235146d5319
KennetTech <64315533+KennetTech@users.noreply.github.com>
2025-05-15 13:17:56 +0200

    feat: added Move class to Models in project

---
+++ Codebreaker.GameAPIs.Models/Move.cs
@@ -0,0 +1,17 @@
+﻿namespace Codebreaker.GameAPIs.Models;
+
+public class Move(Guid id, int moveNumber)
+{
+    public Guid Id { get; set; }
+
+    public int MoveNumber { get; set; } = moveNumber;
+
+    public required string[] GuessPegs { get; init; }
+
+    public required string[] KeyPegs { get; init; }
+
+    public override string  ToString() => $"{MoveNumber}. " +
+        $"{string.Join('#',GuessPegs)} : " +
+        $"{string.Join('#', KeyPegs)}";
+    
+}
